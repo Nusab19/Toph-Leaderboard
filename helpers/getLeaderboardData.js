b14824bb6cdcd69fcb8d5dd0bf913cee92f5755f
Nusab Taha <nusabtaha33@gmail.com>
2024-01-12 10:25:38 +0600

    rollback to latest deployment

--- helpers/getLeaderboardData.js
+++ helpers/getLeaderboardData.js
@@ -1,8 +1,18 @@
 export default async function getLeaderboardData() {
   const rootURL = process.env.ROOT_URL || "http://localhost:3000";
-  const res = await fetch(`${rootURL}/api/leaderboard`, {
-    next: { revalidate: 60 * 10 }, // 10 minutes
-  });
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(`${rootURL}/api/leaderboard`, {
+      next: { revalidate: 60 * 10 }, // 10 minutes
+    });
+    const data = await res.json();
+    return data;
+    
+  } catch (error) {
+    console.log(`Error while fetching leaderboard data: ${error}`);
+    const res = await fetch("https://toph-api.onrender.com/getData", {
+      next: { revalidate: 60 * 10 }, // 10 minutes
+    });
+    const data = await res.json();
+    return data;
+  }
 }
