139f84b3ca46d06bac3f3dfdbf106de1cde84a4c
Nusab Taha <nusabtaha33@gmail.com>
2024-01-13 14:04:42 +0600

    Add goback icon to icons.js

--- helpers/icons.js
+++ helpers/icons.js
@@ -83,6 +83,25 @@ const icons = {
       />
     </svg>
   ),
+  goback: (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      stroke="#e7ecf1"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      strokeWidth="1.9"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" />
+      <g>
+        <path d="M9.00039 15.3802H13.9204C15.6204 15.3802 17.0004 14.0002 17.0004 12.3002C17.0004 10.6002 15.6204 9.22021 13.9204 9.22021H7.15039" />
+        <path d="M8.57 10.7701L7 9.19012L8.57 7.62012" />
+      </g>
+    </svg>
+  ),
 };
 
 export default icons;
