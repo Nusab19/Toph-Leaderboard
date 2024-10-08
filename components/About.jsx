67b7c0385fac1f38390a964f49f64317aa0b2e5c
Nusab Taha <nusabtaha33@gmail.com>
2024-10-08 23:11:51 +0600

    ...

---
+++ components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="mt-8 text-center text-2xl font-semibold">About</h1>
+      <p className="mt-4 text-center">
+        This is not any official Toph.co leaderboard. It was made using the
+        public data from toph.co.
+      </p>
+    </div>
+  );
+};
+
+export default About;
