9ceb641e224866ffff662f16c6142f5e6d6b78fc
Nusab Taha <nusabtaha33@gmail.com>
2025-12-06 02:49:57 +0600

    Fix typo

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,7 @@ const inter = Ubuntu({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata = {
   title: "Toph Leaderboard",
-  metataBase: new URL("https://toph.pages.dev/"),
+  metadataBase: new URL("https://toph.pages.dev/"),
   description:
     "Toph Leaderboard is a website showcasing the users with the most Fastest, Lightest & Shortest code submissions in toph.co online judge",
   keywords:
