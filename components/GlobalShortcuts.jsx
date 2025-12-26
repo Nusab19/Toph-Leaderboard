3bfd381ec7ef517f0a34e967f6110c35b6556c4b
Nusab Taha <85403795+Nusab19@users.noreply.github.com>
2025-12-26 07:49:09 +0600

    Update GlobalShortcuts.jsx

--- components/GlobalShortcuts.jsx
+++ components/GlobalShortcuts.jsx
@@ -20,12 +20,19 @@ export default function GlobalShortcuts() {
           action: () => router.push("/"),
           runOnInput: false,
         },
-        { key: "backspace", action: () => router.back(), runOnInput: false },
-        {
-          key: "alt+backspace",
-          action: () => router.back(),
-          runOnInput: false,
-        },
+          // backspace -> back (only if not already at home)
+  useHotkeys("backspace", (e) => {
+    e.preventDefault();
+    if (pathname !== "/") {
+      router.back();
+    }
+  });
+  useHotkeys("alt+backspace", (e) => {
+    e.preventDefault();
+    if (pathname !== "/") {
+      router.back();
+    }
+  });
         {
           key: "escape",
           action: () => document.activeElement.blur(),
